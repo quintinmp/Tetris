f53f02350ec55000db24fc11e8f6ec40c8f7122e
quintinmp <100251509+quintinmp@users.noreply.github.com>
2025-08-29 17:52:37 -0600

    added camera

---
+++ Camera.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tetris
+{
+    public class Camera
+    {
+
+        private int _screenWidth, _screenHeight;
+        private float _fieldOfView;
+        private Vector3 _position, _target;
+
+        private Matrix _view, _projection;
+
+        private const float nearPlane = 0.01f;
+        private const float farPlane = 100f;
+
+
+        public Camera(Vector3 position, Vector3 target, int screenWidth, int screenHeight, float fieldOfView)
+        {
+            _position = position;
+            _target = target;
+            _screenWidth = screenWidth;
+            _screenHeight = screenHeight;
+            _fieldOfView = fieldOfView;
+
+            CalculateMatrices();
+        }
+
+        private void CalculateMatrices()
+        {
+            _view = Matrix.CreateLookAt(_position, _target, Vector3.Up);
+            float aspect = (float)_screenWidth/(float)_screenHeight;
+            _projection = Matrix.CreatePerspectiveFieldOfView(_fieldOfView, aspect, nearPlane, farPlane);
+        }
+
+        public Matrix View
+        {
+            get { return _view; }
+        }
+
+        public Matrix Projection
+        {
+            get { return _projection; }
+        }
+
+        public void SetCameraPosition(Vector3 position)
+        {
+            _position = position;
+            CalculateMatrices ();
+        }
+
+        public void SetCameraTarget(Vector3 target)
+        {
+            _target = target;
+            CalculateMatrices ();
+        }
+    }
+}
